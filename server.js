85833a71cb08e1b2d83fd9f43792445c2449f1fd
Rahman <rahman.aboujeeb@gmail.com>
2020-01-16 16:15:52 +0800

    connecting with mongodb and inserting posts through the code

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ var http = require('http');
 var server = http.createServer(function (req, res) //creating server
 {
     //handling incoming requests
+    if(req.url == '/')
+    {
+        //setting response header
+        res.writeHead(200, {'Content-Type': 'text/html'});
+
+        //setting response content
+        res.write('This is testing number 1');
+        res.end();
+    }
 
 });
 
