940009b4a71e61bdb62b11af9e02d97a7f11e709
Rahman <rahman.aboujeeb@gmail.com>
2020-01-17 17:20:40 +0800

    get items by id

--- app.js
+++ app.js
@@ -10,16 +10,25 @@ MongoClient.connect(url, function (err, db)
     console.log('db');
     db.collection('Posts', function(err, collection)
     {
-        collection.insert({ id:1, title: 'Title 1', text: 'should be some long text here 1'});
-        collection.insert({ id:2, title: 'Title 2', text: 'should be some long text here 2'});
-        collection.insert({ id:3, title: 'Title 3', text: 'should be some long text here 3'});
+        //collection.insert({ id:1, title: 'Title 1', text: 'should be some long text here 1'});
+        //collection.insert({ id:2, title: 'Title 2', text: 'should be some long text here 2'});
+        //collection.insert({ id:3, title: 'Title 3', text: 'should be some long text here 3'});
 
 
-        db.collection('Posts').count(function(err, count)
+        collection.find({},{"id": ""} ).toArray(
+            function(err, result)
+            {
+                if(err) throw err;
+                console.log(result);
+                
+            }
+        )
+
+        /*db.collection('Posts').count(function(err, count)
         {
             if(err) throw err;
 
             console.log('Total Rows: ' + count);
-        });
+        });*/
     });
 });
